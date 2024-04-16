57f57666bdaeeec87103a5ef55a4728bc742fa93
kiana <kianakermani2001@gmail.com>
2024-04-16 10:21:04 +0430

    navbar done!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,43 @@
 import { FaCheckCircle } from "react-icons/fa";
 import { CiStar } from "react-icons/ci";
+import { CiCirclePlus } from "react-icons/ci";
+import { RiFilterLine } from "react-icons/ri";
+import { GoPulse } from "react-icons/go";
+import { IoIosMenu } from "react-icons/io";
+import { BsThreeDots } from "react-icons/bs";
+import { HiOutlineArrowsUpDown } from "react-icons/hi2";
+import { FaCaretDown } from "react-icons/fa";
 import "../App.css";
 
 const Navbar = () => {
   return (
     <>
-      <div className="bg-white mt-2 h-12 con flex">
+      <div className="bg-white mt-2 h-12 con flex justify-content-between">
         <div className="flex-1 w-64">
-          <FaCheckCircle className="check  ml-6" />
+          <FaCheckCircle className="check  ml-6 fsize" />
           <p className="text ml-4">Reyham (Web Development)</p>
           <CiStar className="star ml-2" />
         </div>
-        <div className="flex-shrink-0">hello</div>
+        <div className="mr-1 over ">
+          <span className="font-bold ml-6 number ">1</span>
+          <span className="font-semibold due">Overdue</span>
+        </div>
+        <div>
+          <span className="line">/</span>
+        </div>
+
+        <div className="mr-1 pendings ">
+          <span className="font-bold ml-6 number ">22</span>
+          <span className="font-semibold pend">Pending</span>
+        </div>
+
+        <FaCaretDown className="mr-6 mt-4" />
+        <HiOutlineArrowsUpDown className="filter mr-2 mt-1" />
+        <IoIosMenu className="filter mr-2 mt-1" />
+        <GoPulse className="filter mr-2 mt-1" />
+        <RiFilterLine className="filter mr-2 mt-1" />
+        <BsThreeDots className="filter mt-1 mr-1" />
+        <CiCirclePlus className="plus mr-2 fsize" />
       </div>
     </>
   );
